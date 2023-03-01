8ebf033f274596c04be7a380b996dc8cd5a25a01
Marc Bayle <marc.scl.bayle@gmail.com>
2023-03-01 14:57:51 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index, :show, :create] do
-    resources :reviews, only: [:new, :create]
-  end
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
